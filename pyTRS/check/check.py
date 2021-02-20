ff2aab65e2bcb40b2ecadeb6ec82aed45720d199
James P. Imes <jamesimes@gmail.com>
2021-02-19 22:10:59 -0800

    list comprehension and better flow control; args -> adhere to PEP-8

--- pyTRS/check/check.py
+++ pyTRS/check/check.py
@@ -5,36 +5,44 @@ Functions for quick-checking some attributes of PLSSDesc or Tract
 objects.
 """
 
-def check_flag(target, flag=None, getLines=False):
+
+def check_flag(target, flag, get_lines=False):
     """
-    Check whether the flagList in `target` (a PLSSDesc object or a Tract
-    object) contains the specified flag (flag='specify string').
-    Optionally returns a list of the lines in the text that raised that
-    warning flag, with getLines=True.
+    Check whether the `.w_flags` or `.e_flags`. list in `target` (a
+    PLSSDesc object or a Tract object) contains the specified `flag`.
+    Optionally return also a list of the lines in the text that raised
+    that flag, with `get_lines=True`.
 
     NOTE: If `target` was passed as a type other than PLSSDesc or Tract,
     this will return None.
+
+    :param target: A pyTRS.Tract or pyTRS.PLSSDesc object.
+    :param flag: A string, specifying the error flag or warning flag
+    that should be looked for.
+    :param get_lines: A bool, specifying whether or not to also return a
+    list of the lines (or context) that raised the requested `flag`.
+    :return: If `get_lines=False` (the default) will return a bool. If
+    `get_lines=True`, then will return a 2-tuple, composed of the bool
+    and a list of the lines or context that raised the `flag`.
     """
 
     from pyTRS.parser import PLSSDesc, Tract
 
     # Feed in only a PLSSDesc object or Tract object.
     if not isinstance(target, (PLSSDesc, Tract)):
-        return None
-
-    if not isinstance(flag, str):
         raise TypeError(
-            "Error: `flag` must be type str, being the name of the flag "
-            "to look for, ex: `flag='secError'`")
+            "`target` must be either a pyTRS.PLSSDesc "
+            "or pyTRS.Tract object"
+        )
 
-    # Get a bool, whether the flag is in either flagList
-    all_flags = target.wFlagList + target.eFlagList
-    flagFound = flag in all_flags
-    if getLines:
-        flagLines = []
-        for lineTuple in all_flags:
-            if lineTuple[0] == flag:
-                flagLines.append(lineTuple[1])
-        return flagFound, flagLines
-    else:
-        return flagFound
+    # Get a bool, whether the flag is in either flag list
+    all_flags = target.w_flags + target.e_flags
+    flag_found = flag in all_flags
+    if not get_lines:
+        return flag_found
+
+    all_flag_lines = target.w_flag_lines + target.e_flag_lines
+    context = [
+        line for flag_, line in all_flag_lines if flag_ == flag
+    ]
+    return flag_found, context
