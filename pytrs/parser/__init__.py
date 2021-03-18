7392377b6ba22c554989cbd63a09bf70f8e5c505
James P. Imes <jamesimes@gmail.com>
2021-03-17 22:47:17 -0700

    cleaned up docstrings

--- pytrs/parser/__init__.py
+++ pytrs/parser/__init__.py
@@ -5,10 +5,10 @@ The main parsing package. Primary classes:
 > PLSSDesc objects parse PLSS description text (full descriptions) into
     Tract objects (one TRS + description per Tract), stored as TractList
 > Tract objects parse tract text into lots and aliquots.
+> TRS objects break a Twp/Rge/Sec into its components.
 > TractList objects contain a list of Tracts, and can compile that Tract
     data into broadly useful formats (i.e. into list, dict, string).
 > Config objects configure parsing parameters for Tract and PLSSDesc.
-> ParseBag objects handle data within / between Tract and PLSSDesc.
 """
 
 from .parser import (
