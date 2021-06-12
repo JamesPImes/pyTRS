cd46934faf2402e96a890f9ad439d5618e5dc3d8
James P. Imes <jamesimes@gmail.com>
2021-06-12 13:17:44 -0700

    update patch version

--- pytrs/_constants.py
+++ pytrs/_constants.py
@@ -1,6 +1,6 @@
 
-__version__ = '1.1.1'
-__version_date__ = '4/23/2021'
+__version__ = '1.1.2'
+__version_date__ = '6/12/2021'
 __author__ = 'James P. Imes'
 __email__ = 'jamesimes@gmail.com'
 __website__ = 'https://github.com/JamesPImes/pyTRS'
