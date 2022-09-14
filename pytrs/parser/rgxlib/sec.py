62047d4e9557b48559ea5e94dfefd7d36d24a0d5
JamesPImes <jamesimes@gmail.com>
2022-09-13 18:46:17 -0600

    refactor/test: sec_regex incorporating no_num_sec_regex

--- pytrs/parser/rgxlib/sec.py
+++ pytrs/parser/rgxlib/sec.py
@@ -5,13 +5,24 @@ Regex patterns for Sections and related preprocessing and parsing.
 
 import re
 
+# Regex pattern for the word "Section" or equivalent symbol, allowing
+# for slight misspellings, and capturing optional plural 's'.
+no_num_sec_regex = re.compile(
+    r"((Section|Sect\.?|Sec\.?|Secion|Seciton|Secton|Sectn|Secn|§)(?P<plural>s)?)",
+    re.IGNORECASE)
+
+# Regex pattern to match "Section <secnum>".
 sec_regex = re.compile(
-    r"""
-    # The word or symbol "Section".
-    ((Section|Sect\.?|Sec\.?|Secion|Secton|Seciton|Sectn|Secn|§)
-    (?P<plural>s)?              # Plural 's' (optional).
+    fr"""
+    (
+    # The word or symbol "Section" (also captures named group 'plural'
+    # if final 's' is present).
+    {no_num_sec_regex.pattern}
     
     [\.\-–—\s]*                 # Deadspace between "Section" and secnum
     
-    (?P<secnum>\d{1,3})\s*)     # Section number, between 1 and 3 digits
+    # Note the double curly brackets to escape f-string syntax.
+    (?P<secnum>\d{{1,3}})       # Section number, between 1 and 3 digits.
+    \s*                         # Trailing whitespace.
+    )
     """, re.IGNORECASE | re.VERBOSE)
