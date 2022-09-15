c053d7c2dbf3e148ac32e0fd171401cb8c6d9ad4
JamesPImes <jamesimes@gmail.com>
2022-09-14 22:06:10 -0600

    refactor/test: is_multi_sec

--- pytrs/parser/unpackers.py
+++ pytrs/parser/unpackers.py
@@ -13,13 +13,31 @@ from .rgxlib import *
 def is_multi_lot(multilots_mo) -> bool:
     """
     INTERNAL USE:
-    Whether a multilot_regex (or multilot_with_lots_regex) match object
-    found more than one lot (True) or if it is only one lot (False).
+    Whether a multilot_regex (or multilot_with_aliquots_regex) match
+    object found more than one lot (True) or if it is only one lot
+    (False).
 
     WARNING: Do not pass a match object for any other regex pattern.
     """
     if multilots_mo['lotnum_rightmost'] is not None:
         return True
     elif multilots_mo['lotnum'] is not None:
+        return False
+    raise ValueError
+
+
+# Unpacking multisec_regex.
+
+def is_multi_sec(multisec_mo) -> bool:
+    """
+    INTERNAL USE:
+    Whether a multisec_regex match object found more than one lot (True)
+    or if it is only one lot (False).
+
+    WARNING: Do not pass a match object for any other regex pattern.
+    """
+    if multisec_mo['secnum_rightmost'] is not None:
+        return True
+    elif multisec_mo['secnum'] is not None:
         return False
     raise ValueError
--- tests/test_unpackers.py
+++ tests/test_unpackers.py
@@ -8,14 +8,22 @@ import unittest
 try:
     from pytrs.parser.rgxlib import *
     from pytrs.parser.unpackers import (
+        # lot/multilot functions
         is_multi_lot,
+
+        # sec/multisec functions
+        is_multi_sec,
     )
 except ImportError:
     import sys
     sys.path.append('../')
     from pytrs.parser.rgxlib import *
     from pytrs.parser.unpackers import (
+        # lot/multilot functions
         is_multi_lot,
+
+        # sec/multisec functions
+        is_multi_sec,
     )
 
 
@@ -37,3 +45,19 @@ class LotUnpackersTests(unittest.TestCase):
         no_aq_mo = multilot_with_aliquot_regex.search(no_with_aliquot)
         self.assertFalse(is_multi_lot(no_aq_mo))
 
+
+class SecUnpackersTests(unittest.TestCase):
+
+    def test_is_multi_sec(self):
+        no = 'Sec 1'
+        yes = 'Sec 1 and 3 - 5'
+
+        yes_mo = multisec_regex.search(yes)
+        self.assertTrue(is_multi_sec(yes_mo))
+
+        no_mo = multisec_regex.search(no)
+        self.assertFalse(is_multi_sec(no_mo))
+
+
+if __name__ == '__main__':
+    unittest.main()
