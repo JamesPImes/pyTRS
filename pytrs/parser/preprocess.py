64a29e70a99f573606cc494d0ea72ee3f2b45bbb
JamesPImes <jamesimes@gmail.com>
2022-09-15 04:22:00 -0600

    refactor/test: scrub_aliquots and associated sub-functions

--- pytrs/parser/preprocess.py
+++ pytrs/parser/preprocess.py
@@ -2,6 +2,121 @@
 from .rgxlib import *
 from .unpackers import *
 
+# Clean aliquot abbreviations with fraction.
+NE_FRAC = 'NE¼'
+NW_FRAC = 'NW¼'
+SE_FRAC = 'SE¼'
+SW_FRAC = 'SW¼'
+N2_FRAC = 'N½'
+S2_FRAC = 'S½'
+E2_FRAC = 'E½'
+W2_FRAC = 'W½'
+
+# Define what should replace matches of each regex that is used in the
+# scrub_aliquots() method.
+QQ_SCRUBBER_DEFINITIONS = {
+    ne_regex: NE_FRAC,
+    nw_regex: NW_FRAC,
+    se_regex: SE_FRAC,
+    sw_regex: SW_FRAC,
+    n2_regex: N2_FRAC,
+    s2_regex: S2_FRAC,
+    e2_regex: E2_FRAC,
+    w2_regex: W2_FRAC,
+    ne_clean: NE_FRAC,
+    nw_clean: NW_FRAC,
+    se_clean: SE_FRAC,
+    sw_clean: SW_FRAC,
+}
+
+# The basic scrubbing regexes.
+SCRUBBER_REGEXES = (
+    ne_regex,
+    nw_regex,
+    se_regex,
+    sw_regex,
+    n2_regex,
+    s2_regex,
+    e2_regex,
+    w2_regex,
+)
+
+# Optionally addable scrubbing regexes.
+CLEAN_QQ_REGEXES = (
+    ne_clean,
+    nw_clean,
+    se_clean,
+    sw_clean,
+)
+
+
+def sub_scrubber(txt, scrubber_rgx):
+    """
+    Convert the raw aliquots to cleaner components, using the
+    specified scrubber_rgx.
+    """
+    replace_with = QQ_SCRUBBER_DEFINITIONS[scrubber_rgx]
+    # Make substitutions until there are no changes.
+    new_txt = ''
+    while txt != new_txt:
+        new_txt = txt
+        txt = re.sub(scrubber_rgx, replace_with, txt)
+    return new_txt
+
+
+def half_plus_q_scrubber(txt):
+    """
+    Scrub patterns like 'E½NENW' into 'E½NE¼NW¼', even without clean_qq.
+    (Requires a leading half with fraction.)
+    """
+    new_txt = ''
+    while txt != new_txt:
+        new_txt = txt
+        mo = half_plus_q_regex.search(txt)
+        if mo is None:
+            continue
+        rightmost_comparer = mo['quarter_aliquot_rightmost']
+        rightmost_quarter = ''
+        if mo['ne_found'] == rightmost_comparer:
+            rightmost_quarter = 'NE¼'
+        elif mo['nw_found'] == rightmost_comparer:
+            rightmost_quarter = 'NW¼'
+        elif mo['se_found'] == rightmost_comparer:
+            rightmost_quarter = 'SE¼'
+        elif mo['sw_found'] == rightmost_comparer:
+            rightmost_quarter = 'SW¼'
+        i = mo.start('quarter_aliquot_rightmost')
+        replace_with = f"{mo.group(0)[:i]}{rightmost_quarter}"
+        txt = re.sub(half_plus_q_regex, replace_with, txt)
+    return txt
+
+
+def scrub_aliquots(txt, clean_qq=False):
+    """
+    INTERNAL USE:
+
+    Convert non-standard aliquots into standard abbreviations with
+    appropriate fractions.
+
+    For example, 'Northeast Quarter' or 'NE/4' -> 'NE¼'.
+
+    :param txt:
+    :param clean_qq: Whether to apply the so-called ``clean_qq`` regex
+    preprocessing (e.g., interpret 'NE' as 'NE¼'.)
+    :return: The original string, with aliquots converted to standard
+    abbreviations with appropriate fractions.
+    """
+    for rgx in SCRUBBER_REGEXES:
+        txt = sub_scrubber(txt, rgx)
+    if clean_qq:
+        for rgx in CLEAN_QQ_REGEXES:
+            txt = sub_scrubber(txt, rgx)
+    # Scrub 'E½NENW' -> 'E½NE¼NW¼'.
+    txt = half_plus_q_scrubber(txt)
+    # Remove any intervening 'of the' and whitespace.
+    txt = remove_aliquot_interveners(txt)
+    return txt
+
 
 def remove_aliquot_interveners(txt):
     """
@@ -22,3 +137,4 @@ def remove_aliquot_interveners(txt):
             txt)
 
     return txt
+
--- tests/test_preprocess_functions.py
+++ tests/test_preprocess_functions.py
@@ -5,12 +5,14 @@ import unittest
 try:
     from pytrs.parser.preprocess import (
         remove_aliquot_interveners,
+        scrub_aliquots,
     )
 except ImportError:
     import sys
     sys.path.append('../')
     from pytrs.parser.preprocess import (
         remove_aliquot_interveners,
+        scrub_aliquots,
     )
 
 
@@ -28,6 +30,31 @@ class PreprocessFuncTestCase(unittest.TestCase):
         }
         for txt, expected in txts_expected.items():
             self.assertEqual(expected, remove_aliquot_interveners(txt))
+
+    def test_scrub_aliquots(self):
+        txts_expected = {
+            'Lot 1 of SE/4 of the NW/4': 'Lot 1 of SE¼NW¼',
+            'Southeast Quarter of the Northeast Quarter': 'SE¼NE¼',
+            'One Hundred Feet': 'One Hundred Feet',
+            'NENE': 'NENE',
+            'S2NE': 'S½NE¼',
+            'S2NENW': 'S½NE¼NW¼',
+            'N2 of NE of NW, NW': 'N½NE¼NW¼, NW',
+        }
+        for txt, expected in txts_expected.items():
+            self.assertEqual(expected, scrub_aliquots(txt))
+
+    def test_scrub_aliquots_clean_qq(self):
+        txts_expected = {
+            'Lot 1 of SE/4 of the NW/4': 'Lot 1 of SE¼NW¼',
+            'Southeast Quarter of the Northeast Quarter': 'SE¼NE¼',
+            'NENE': 'NE¼NE¼',
+            'S2NE': 'S½NE¼',
+            'S2NENW': 'S½NE¼NW¼',
+            'N2 of NE of NW, NW': 'N½NE¼NW¼, NW¼',
+        }
+        for txt, expected in txts_expected.items():
+            self.assertEqual(expected, scrub_aliquots(txt, clean_qq=True))
 
 
 if __name__ == '__main__':
