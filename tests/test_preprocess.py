eaac4bfc1b49b4abfa9053e136b95ad2720c4c7a
JamesPImes <jamesimes@gmail.com>
2022-09-15 15:57:58 -0600

    refactor/test: class and functions for preprocessing full PLSS descriptions before parsing

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,17 +1,21 @@
 
+"""
+Tests for the pytrs.parser.plss_preprocess submodule.
+"""
+
 import unittest
 
 try:
-    import pytrs
-    from pytrs.parser.parser import (
+    from pytrs.parser.plss_preprocess import (
         PLSSPreprocessor,
+        find_twprge,
     )
 except ImportError:
     import sys
     sys.path.append('../')
-    import pytrs
-    from pytrs.parser.parser import (
+    from pytrs.parser.plss_preprocess import (
         PLSSPreprocessor,
+        find_twprge,
     )
 
 
@@ -36,7 +40,67 @@ class PreprocessTest(unittest.TestCase):
         expected = 'T154N-R97W Sec 14: NE/4'
         self.assertEqual(expected, PLSSPreprocessor(txt).text)
 
-    # TODO: test_ocr_scrub
+    def test_ocr_scrub(self):
+        txt = """Township lS4 North, Range 97 West
+        Section 14: NE/4
+        Township 1SS North, Range 97 West
+        Sec 22: ALL"""
+        expected = """T154N-R97W Section 14: NE/4
+        T155N-R97W Sec 22: ALL"""
+
+        # Get rid of indentation in multi-line strings.
+        txt = txt.replace(' ' * 8, '')
+        expected = expected.replace(' ' * 8, '')
+        self.assertEqual(expected, PLSSPreprocessor(txt, ocr_scrub=True).text)
+
+    def test_find_twprge_basic(self):
+        txt = """Township 154 North, Range 97 West
+            Section 14: NE/4
+            T 155 N, R 97 W
+            Section 22: ALL
+            156N-97W
+            Sec 1: Lots 1 - 3
+            T1S-R9E"""
+        expected = [
+            'T154N-R97W',
+            'T155N-R97W',
+            'T156N-R97W',
+            'T1S-R9E'
+        ]
+        self.assertEqual(expected, find_twprge(txt))
+
+    def test_find_twprge_preprocess_default(self):
+        txt = """Township 154, Range 97 West
+            Section 14: NE/4
+            T 155 N, R 97
+            Section 22: ALL
+            T156-R97
+            Sec 1: Lots 1 - 3
+            T1S-R9E"""
+        expected = [
+            'T154N-R97W',
+            'T155N-R97W',
+            'T156N-R97W',
+            'T1S-R9E'
+        ]
+        self.assertEqual(expected, find_twprge(txt, preprocess=True))
+
+    def test_find_twprge_preprocess_se(self):
+        txt = """Township 154, Range 97 West
+            Section 14: NE/4
+            T 155 N, R 97
+            Section 22: ALL
+            T156-R97
+            Sec 1: Lots 1 - 3
+            T1S-R9E"""
+        expected = [
+            'T154S-R97W',
+            'T155N-R97E',
+            'T156S-R97E',
+            'T1S-R9E'
+        ]
+        self.assertEqual(expected, find_twprge(
+            txt, default_ns='s', default_ew='e', preprocess=True))
 
 
 if __name__ == '__main__':
