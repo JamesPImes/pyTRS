bebba92d249f51d60398174b047faaa210c91669
JamesPImes <jamesimes@gmail.com>
2022-09-18 15:05:37 -0600

    run all tests

---
+++ tests/test_all.py
@@ -0,0 +1,16 @@
+
+"""
+Run all tests.
+"""
+
+import unittest
+
+from test_regex import *
+from test_unpackers import *
+from test_tract_preprocess import *
+from test_tract_and_parse import *
+from test_plss_preprocess import *
+from test_plss_and_parse import *
+
+if __name__ == '__main__':
+    unittest.main()
