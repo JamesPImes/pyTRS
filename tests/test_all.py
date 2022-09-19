4bf43b9bc5d7c9ebd48109c2f6b54d2573e4d47f
JamesPImes <jamesimes@gmail.com>
2022-09-18 21:51:33 -0600

    fix: correct import

--- tests/test_all.py
+++ tests/test_all.py
@@ -10,7 +10,7 @@ from test_unpackers import *
 from test_tract_preprocess import *
 from test_tract_and_parse import *
 from test_plss_preprocess import *
-from test_plss_and_parse import *
+from test_plssdesc_and_parse import *
 from test_trs import *
 
 if __name__ == '__main__':
