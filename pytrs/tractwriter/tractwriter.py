33b580fcd2fdafdb7b956ef2a4c3996c012bdf1f
JamesPImes <jamesimes@gmail.com>
2022-09-20 21:30:33 -0600

    clean up docstrings for rST

--- pytrs/tractwriter/tractwriter.py
+++ pytrs/tractwriter/tractwriter.py
@@ -9,62 +9,75 @@ from ..utils import gen_uid
 
 class TractWriter:
     """
-    A wrapper for csv.writer for streamlined output of Tract data.
+    A wrapper for builtin ``csv.writer`` for streamlined output of
+    ``Tract`` data. Ensures that the same attributes are written for
+    every ``Tract``.
     """
     def __init__(
             self, attributes, fp, mode, plus_cols=None, nice_headers=False,
             uid: int = None):
         """
-        A wrapper for csv.writer for streamlined output of Tract data.
-        Ensures that the same attributes are written for every Tract
-        processed.
+        A wrapper for ``csv.writer`` for streamlined output of ``Tract``
+        data.
 
-        :param attributes: The Tract attributes to write for each Tract.
+        :param attributes: The ``Tract`` attributes to write for each
+        ``Tract``. (See documentation for ``Tract`` class for all
+        important attributes.)
+
         :param fp: Filepath to the .csv file to write to.
-        :param mode: Whether to open the file in mode 'w' or 'a'.
-        Defaults to 'w'. (But if a file is closed with ``.close()`` and
-        later reopened with ``.open()``, it will be reopened in 'a'
-        mode.)
+
+        :param mode: Whether to open the file in mode ``'w'`` or
+         ``'a'``. Defaults to ``'w'``. (But if a file is closed with
+         ``.close()`` and later reopened with ``.open()``, it will be
+         reopened in ``'a'`` mode.)
+
         :param plus_cols:  (Optional) a list of additional headers to
-        write that are not covered by the Tract attributes. (If using
-        this functionality, a list of the specific data to write in
-        these columns will need to be included every time something is
-        written.)
+         write that are not covered by the ``Tract`` attributes. (If
+         using this functionality, a list of the specific data to write
+         in these columns will need to be included every time something
+         is written.)
+
         :param nice_headers: By default, this class will use the
-        attribute names as headers. To use custom headers, pass to
-        ``nice_headers=`` any of the following:
-        -- a list of strings to use. (Should be equal in length to the
-        list passed as ``attributes``, but will not raise an error if
-        that's not the case. The resulting column headers will just be
-        fewer than the actual number of columns.)
-        -- a dict, keyed by attribute name, and whose values are the
-        corresponding headers. (Any missing keys will use the attribute
-        name.)
-        -- `True` -> use the values in the ``Tract.ATTRIBUTES`` dict for
-        headers. (WARNING: Any value passed that is not a list or dict
-        and that evaluates to `True` will cause this behavior.)
-        -- If not specified (i.e. None), will just use the attribute
-        names themselves.
+         attribute names as headers. To use custom headers, pass to
+         ``nice_headers=`` any of the following:
+
+         - a list of strings to use. (Should be equal in length to the
+           list passed as ``attributes``, but will not raise an error
+           if that's not the case. The resulting column headers will
+           just be fewer than the actual number of columns.)
+
+         - a dict, keyed by attribute name, and whose values are the
+           corresponding headers. (Any missing keys will use the
+           attribute name.)
+
+         - ``True`` -- use the values in the ``Tract.ATTRIBUTES`` dict
+           for headers. (WARNING: Any value passed that is not a list or
+           dict and that evaluates to ``True`` will cause this
+           behavior.)
+
+         - If not specified (i.e. ``None`` or ``False``), will just use
+           the attribute names themselves (default).
+
         :param plus_cols: (Optional) a list of additional headers to
-        write that are not covered by the Tract attributes.
+         write that are not covered by the ``Tract`` attributes.
 
         :param uid: (Optional) The number at which to start generating
-        unique identifiers. If specified, UID's will be written for
-        every new row (e.g., `'0001.a-e'`). The number component of the
-        UID will be incremented every time `.write()` is called, and the
-        letter components will encode how many rows were added that
-        time (a: 1, b: 2, c: 3, <...>, aa: 27, ab: 28, etc.).
-            For example (assuming our UID is currently at 27, and
-            `parsed_desc` is a `PLSSDesc` object containing 4 Tract
-            objects):
+         unique identifiers. If specified, UID's will be written for
+         every new row (e.g., ``'0001.a-e'``). The number component of
+         the UID will be incremented every time ``.write()`` is called,
+         and the letter components will encode how many rows were added
+         that time (``a: 1, b: 2, c: 3, <...>, aa: 27, ab: 28, etc.``).
 
-             `some_tractwriter.write(parsed_desc)`
-                 -> Writes 4 rows and generates these UIDs (one for
-                 each):
-                     '0027.a-d'
-                     '0027.b-d'
-                     '0027.c-d'
-                     '0027.d-d'
+         For example (assuming our UID is currently at 27, and
+         ``parsed_desc`` is a ``PLSSDesc`` object containing 4 ``Tract``
+         objects):
+
+             .. code-block:: python
+
+                 some_tractwriter.write(parsed_desc)
+                 # Wrote 4 rows and generated these UIDs (one for
+                 # each row)...
+                 # '0027.a-d',  '0027.b-d',  '0027.c-d',  and '0027.d-d'
         """
         self.attributes = attributes
         self.fp = Path(fp)
@@ -122,11 +135,11 @@ class TractWriter:
         """
         Write the data from ``to_write`` to the csv.
         :param to_write: a ``Tract``, a ``PLSSDesc`` object, a
-        ``TractList``, or an iterable of any combination of those object
-        types.
+         ``TractList``, or an iterable containing any number and
+         combination of those object types.
         :param plus_cols: (Optional) a list of additional data to write
-        for each of the written rows. (Will write the same data for
-        every row written.)
+         for each of the written rows. (Will write the same data for
+         every row written.)
         :return: The number of rows written (an int).
         """
         if not self.is_open:
